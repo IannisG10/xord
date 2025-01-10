d1ccbcfaa51f657c2f4b449f9f5a2fb9360bf98d
IannisG10 <guerraiannis@gmail.com>
2025-01-10 07:51:14 +0100

    add sideBar in client DashBoard

--- src/pages/ClientDashBoard/ClientDashBoard.tsx
+++ src/pages/ClientDashBoard/ClientDashBoard.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import ClientList from "@/components/ClientList/ClientList";
+import SideBar from "@/components/SideBar/SideBar";
 import { Button } from "@/components/ui/button";
 import { HiFilter } from "react-icons/hi";
 import { HiUserGroup } from "react-icons/hi2";
@@ -22,23 +23,25 @@ const ClientDashBoard: React.FC = () => {
     },[])
 
     return(
-        <div className="flex flex-col gap-7">
-            <SearchBar/>
-            <div className="bg-[#8fdc97] w-1/6 h-32 font-chivo text-xl rounded-md m-2 p-2">
-                <div>
-                    <HiUserGroup/> Nombre total de clients
+        <div className="h-full flex">
+            <SideBar/>
+            <div className="flex flex-col gap-7 flex-1">
+                <SearchBar/>
+                <div className="bg-[#8fdc97] w-1/6 h-32 font-chivo text-xl rounded-md m-2 p-2">
+                    <div>
+                        <HiUserGroup/> Nombre total de clients
+                    </div>
+                    <div className="m-1">
+                        {status ? client.length : <RxBorderDashed/>}
+                    </div>
                 </div>
-                <div className="m-1">
-                    {status ? client.length : <RxBorderDashed/>}
+                <div className="flex justify-end mr-2">
+                    <Button className="w-1/12" variant="outline">
+                        <HiFilter/> Filtrer
+                    </Button>
                 </div>
+                <ClientList/>
             </div>
-            
-            <div className="flex justify-end mr-2">
-                <Button className="w-1/12" variant="outline">
-                    <HiFilter/> Filtrer
-                </Button>
-            </div>
-            <ClientList/>
         </div>
     )
 }
